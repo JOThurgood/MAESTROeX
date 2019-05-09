074136ecc75e11415b0786a4fd52d020562ccb24
Jonathan Thurgood <njt132@salem.campus.ncl.ac.uk>
2019-05-09 12:57:49 +0100

    basic circular drop setup - not sure about BC and 2 level has issues

---
+++ Exec/TEST_PROBLEMS/circular_drop/cleanout.sh
@@ -0,0 +1,13 @@
+# clean up various output
+# be carefull with this one...
+
+rm -r chk*
+rm -r plt* 
+rm -r smallplt* 
+rm -rf single_level
+rm -rf smallscale_single_level
+rm -rf 2level
+rm -rf tmp_build_dir
+rm *.out
+rm *.0
+rm movie.visit
