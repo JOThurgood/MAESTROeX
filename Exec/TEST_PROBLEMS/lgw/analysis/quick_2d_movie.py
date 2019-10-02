11bb13fd8e5ac91d008cd46ad850da5ddcc9fb48
Jonathan Thurgood <njt132@salem.campus.ncl.ac.uk>
2019-10-02 13:10:05 +0100

    add option to make plots for all variables in quick_2d_movie.py

--- Exec/TEST_PROBLEMS/lgw/analysis/quick_2d_movie.py
+++ Exec/TEST_PROBLEMS/lgw/analysis/quick_2d_movie.py
@@ -23,11 +23,30 @@ def quick2DMov(var,tsPath,outDir):
 
     return
 
+# setup the command line parser arguments
 parser = argparse.ArgumentParser("quick_2d_movie")
-parser.add_argument("varname", help="name of variable to save (eg. rho)", type=str)
+parser.add_argument("varname", help="name of variable to save (eg. rho). Set to 'all' to attempt to make 2d movies of all plotfile variables", type=str)
 parser.add_argument("plotfiles_path", help="path to directory containing a series of plotfiles", type=str)
 parser.add_argument("basename", help="base name of the plotfiles (i.e., before the numbers, eg. plt)", type=str)
 parser.add_argument("output_path",    help="path to save directory", type=str)
 args = parser.parse_args()
 
-quick2DMov(args.varname, args.plotfiles_path+args.basename+'???????/', args.output_path)
+# Main
+
+tsPath = args.plotfiles_path+args.basename+'???????/'
+
+if args.varname == "all" :
+    ts = yt.load(tsPath)
+    ds0 = ts[0]
+    for item in ds0.field_list:
+        var = item[1]
+        print('saving var={}'.format(var))
+        try:
+            quick2DMov(var, tsPath, args.output_path)
+        except:
+            print('something went wrong with saving var={}, (possibly to do with escaping special characters)'.format(var))
+        pass       
+
+else:
+    quick2DMov(args.varname, tsPath, args.output_path)
+
